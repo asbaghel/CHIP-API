6268854666c661b5746fc815b17bb8c5fdf241a8
asbaghel <abhisheksbaghel99@gmail.com>
2020-12-18 01:11:14 +0530

    Added cors and db pass

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-
-const { Client } = require('pg');
+const cors = require("cors");
+const { Client } = require("pg");
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'myfirstdb',
-  password: 'rootpasswd',
+  user: "postgres",
+  host: "localhost",
+  database: "myfirstdb",
+  password: "admin123",
   port: 5432,
 });
 client.connect().then(() => {
@@ -15,20 +15,22 @@ client.connect().then(() => {
 
 const app = express();
 
+app.use(cors());
 app.set("json spaces", 2);
 app.use(bodyParser.json({ limit: "15mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/master', (req, res) => {
+app.post("/api/master", (req, res) => {
   let query = req.body.query;
   console.log({ query });
 
-  client.query(query)
-    .then(result => {
+  client
+    .query(query)
+    .then((result) => {
       console.log({ query, result });
       res.json({ res: result });
     })
-    .catch(err => {
+    .catch((err) => {
       console.error({ query, err });
       res.status(500).json({ err });
     });
